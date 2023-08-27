781e43c811d04743e0918cc26e656eefcd40d6d8
Lucas Pereira <lucasgonp@gmail.com>
2023-08-27 19:37:15 -0300

    bug fix

--- Modules/Features/Authenticator/Authenticator/Helpers/Validations/EmailValidation.swift
+++ Modules/Features/Authenticator/Authenticator/Helpers/Validations/EmailValidation.swift
@@ -1,6 +1,6 @@
 import DesignKit
 
-struct EmailValidation: Validation {
+final class EmailValidation: Validation {
     func run(_ text: String) -> ValidationResult {
         if text.isEmpty {
             return .error(Strings.Error.Field.emailEmpty)
